a1abf72d9acb2b4fe1ae5dcf88196b55e42f5342
ducduong303 <57469702+ducduong303@users.noreply.github.com>
2020-11-16 18:14:28 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb25df72c4ef2001e3261e4",
+				"5fb25df72c4ef2001e3261e6"
+			]
+		},
+		"5fb25df72c4ef2001e3261e4": {
+			"id": "5fb25df72c4ef2001e3261e4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb25df72c4ef2001e3261e6": {
+			"id": "5fb25df72c4ef2001e3261e6",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
